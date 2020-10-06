021b55e7af2cfc66a55d1ab5c06ff53d9de4b299
rubentharner <s1144640@student.windesheim.nl>
2020-10-06 21:06:15 +0200

    4.1 nu

--- Race Simulator/ControllerTest/Model_Competition_NextTrackShould.cs
+++ Race Simulator/ControllerTest/Model_Competition_NextTrackShould.cs
@@ -27,5 +27,62 @@ namespace ControllerTest
 
         }
 
+        [Test]
+        public void NextTrack_OneInQueue_ReturnTrack()
+        {
+            SectionTypes[] sections1 = new SectionTypes[]
+            {
+                SectionTypes.Straight,
+                SectionTypes.LeftCorner,
+                SectionTypes.RightCorner,
+                SectionTypes.StartGrid,
+                SectionTypes.Finish
+            };
+            Track track = new Track("Track 1", sections1);
+            _competition.Tracks.Enqueue((track));
+            var result = _competition.NextTrack();
+            Assert.AreEqual(track,result);
+
+        }
+
+        [Test]
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            SectionTypes[] sections1 = new SectionTypes[]
+            {
+                SectionTypes.Straight,
+                SectionTypes.LeftCorner,
+                SectionTypes.RightCorner,
+                SectionTypes.StartGrid,
+                SectionTypes.Finish
+            };
+            Track track = new Track("Track 1", sections1);
+            _competition.Tracks.Enqueue((track));
+            var result = _competition.NextTrack();
+            result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NextTrack_TwoInQueue_ReturnNextTrack()
+        {
+            SectionTypes[] sections1 = new SectionTypes[]
+            {
+                SectionTypes.Straight,
+                SectionTypes.LeftCorner,
+                SectionTypes.RightCorner,
+                SectionTypes.StartGrid,
+                SectionTypes.Finish
+            };
+            Track track = new Track("Track 1", sections1);
+            Track track2 = new Track("Track 2", sections1);
+            _competition.Tracks.Enqueue(track);
+            _competition.Tracks.Enqueue(track2);
+            var result = _competition.NextTrack();
+            Assert.AreEqual(track, result);
+            result = _competition.NextTrack();
+            Assert.AreEqual(track2,result);
+
+        }
     }
 }
