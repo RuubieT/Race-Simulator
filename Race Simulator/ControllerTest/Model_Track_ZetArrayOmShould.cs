ea5b06f7229c87390c03a59c18783f8d03888095
Ruben Tharner <136126ruben@gmail.com>
2020-12-14 21:00:57 +0100

    1 test

---
+++ Race Simulator/ControllerTest/Model_Track_ZetArrayOmShould.cs
@@ -0,0 +1,35 @@
+﻿using Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    class Model_Track_ZetArrayOmShould
+    {
+        private Track track;
+
+        [SetUp]
+        public void SetUp()
+        {
+            SectionTypes[] test = new SectionTypes[]
+            {
+                SectionTypes.StartGridHorizontal, SectionTypes.FinishHorizontal, SectionTypes.WestZuid, SectionTypes.StraightVertical, SectionTypes.StraightVertical, SectionTypes.NoordWest, SectionTypes.StraightHorizontal,
+                SectionTypes.StraightHorizontal, SectionTypes.OostNoord, SectionTypes.StraightVertical, SectionTypes.StraightVertical, SectionTypes.ZuidOost
+            };
+            track = new Track("Test", test);
+        }
+
+        [Test]
+        public void ZetArrayOm_EmptyLinkedList_ReturnEmpty()
+        {
+            var EmptyLinkedList = new SectionTypes[] { };
+            var result = track.ZetArrayOm(EmptyLinkedList);
+            Assert.IsEmpty(result);
+        }
+
+      
+    }
+}
